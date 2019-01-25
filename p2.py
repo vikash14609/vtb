d1a2b010b5c387ad0b28dca3384b2a2d6af9b9c8
vikash14609 <46258749+vikash14609@users.noreply.github.com>
2019-01-25 22:56:35 +0530

    Add files via upload

---
+++ p2.py
@@ -0,0 +1,44 @@
+from time import sleep
+import sys
+def hola():
+    class color:
+     PURPLE = '\033[95m'
+     CYAN = '\033[96m'
+     DARKCYAN = '\033[36m'
+     BLUE = '\033[94m'
+     GREEN = '\033[92m'
+     YELLOW = '\033[93m'
+     RED = '\033[91m'
+     BOLD = '\033[1m'
+     UNDERLINE = '\033[4m'
+     END = '\033[0m'
+    print("\033[1;31;40m SOLD by Telegrammer:  @rohantripathi0096 Warning : THIS TOOL IS JUST FOR EDUCATIONAL PURPOSE AND TO CHECK WEBSITE SECURITY \033[0m")
+    print ("///////////////////////////////////////////////////////////////////////////////////////////")
+    line_1 = "As a young boy, I was taught in high school that hacking was cool. I am vikashthebrainer. You can follow me on @vtbthebrainer on instagram. Join @iwastold on Telegram"
+    for x in line_1:
+     print(x, end='')
+     sys.stdout.flush()
+     sleep(0.01)
+def coda():
+ print ("HOLA")
+def dua():
+ import uuid
+ import hashlib
+ gingercid = (hex(uuid.getnode()))
+ gingervalid = hashlib.md5(gingercid.encode())   
+ gingervalidp = gingervalid.hexdigest()
+ logc = ''
+ 
+ 
+ if gingervalidp == logc :
+    print ('DEVICE CONFIRMED')
+ else :
+    print ('Your Device is not registered with us. CONTACT MY CERTIFIED SELLER  @rohantripathi0096')
+   
+    
+def rep() :
+   repw = input ("Do you want to continue with Amazon")
+    
+    
+    
+    
